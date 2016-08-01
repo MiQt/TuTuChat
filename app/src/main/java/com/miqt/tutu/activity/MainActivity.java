d4767c9d6d252594a64ab641612e19dcb652eefd
MiTrace <miqingtang@163.com>
2016-08-01 11:41:58 +0800

    frist upload

---
+++ app/src/main/java/com/miqt/tutu/activity/MainActivity.java
@@ -0,0 +1,87 @@
+package com.miqt.tutu.activity;
+
+import turing.os.http.core.ErrorMessage;
+import turing.os.http.core.HttpConnectionListener;
+import turing.os.http.core.RequestResult;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.miqt.tlchat.tulingchat.R;
+import com.turing.androidsdk.InitListener;
+import com.turing.androidsdk.SDKInit;
+import com.turing.androidsdk.SDKInitBuilder;
+import com.turing.androidsdk.TuringApiManager;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private TuringApiManager mTuringApiManager;
+
+    Button bt_send;
+    TextView tv_results;
+    EditText et_message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt_send = (Button) findViewById(R.id.bt_send);
+        tv_results = (TextView) findViewById(R.id.tv_results);
+        et_message = (EditText) findViewById(R.id.et_message);
+        bt_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = et_message.getText().toString();
+                if (message != null && !message.equals("")) {
+                    mTuringApiManager.requestTuringAPI(message);
+                }
+            }
+        });
+        init();
+    }
+
+    /**
+     * 初始化turingSDK、识别和tts
+     */
+    private void init() {
+        // turingSDK初始化
+        SDKInitBuilder builder = new SDKInitBuilder(this)
+                .setSecret("d257a9b9d5b47d5f").setTuringKey("a9776345bca744c288d8e14a5322939f").setUniqueId("123");
+        SDKInit.init(builder, new InitListener() {
+            @Override
+            public void onFail(String error) {
+                Log.d(TAG, error);
+            }
+
+            @Override
+            public void onComplete() {
+                // 获取userid成功后，才可以请求Turing服务器，需要请求必须在此回调成功，才可正确请求
+                mTuringApiManager = new TuringApiManager(MainActivity.this);
+                mTuringApiManager.setHttpListener(myHttpConnectionListener);
+            }
+        });
+    }
+
+    /**
+     * 网络请求回调
+     */
+    HttpConnectionListener myHttpConnectionListener = new HttpConnectionListener() {
+
+        @Override
+        public void onSuccess(RequestResult result) {
+            if (result != null) {
+                tv_results.setText(result.getContent().toString());
+            }
+        }
+
+        @Override
+        public void onError(ErrorMessage errorMessage) {
+            Log.d(TAG, errorMessage.getMessage());
+        }
+    };
+}
---
+++ app/src/main/java/com/miqt/tutu/app/MyAppcation.java
@@ -0,0 +1,17 @@
+package com.miqt.tutu.app;
+
+import android.app.Application;
+
+import com.turing.androidsdk.InitListener;
+import com.turing.androidsdk.SDKInit;
+import com.turing.androidsdk.SDKInitBuilder;
+
+/**
+ * Created by IEZ on 2016/8/1.
+ */
+public class MyAppcation extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
