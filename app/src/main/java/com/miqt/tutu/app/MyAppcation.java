2d5bb251ba672bd793d4f8832963c5c40ed471d0
MiTrace <miqingtang@163.com>
2016-08-01 16:54:03 +0800

    bmob init success.

--- app/src/main/java/com/miqt/tutu/app/MyAppcation.java
+++ app/src/main/java/com/miqt/tutu/app/MyAppcation.java
@@ -1,17 +1,28 @@
 package com.miqt.tutu.app;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.miqt.tlchat.tulingchat.BuildConfig;
+import com.miqt.tutu.activity.MainActivity;
 import com.turing.androidsdk.InitListener;
 import com.turing.androidsdk.SDKInit;
 import com.turing.androidsdk.SDKInitBuilder;
+import com.turing.androidsdk.TuringApiManager;
+
+
+import cn.bmob.v3.Bmob;
 
 /**
  * Created by IEZ on 2016/8/1.
  */
 public class MyAppcation extends Application {
+    private static final String TAG = "MyAppcation";
+
     @Override
     public void onCreate() {
         super.onCreate();
+        // 初始化  SDK
+        Bmob.initialize(this, "31558ea36a284e51b5a572035f7e277c");
     }
 }
